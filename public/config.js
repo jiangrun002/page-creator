d7cb47defaa7b4c960496bd3dc998a62c1271154
jiangr <451183365@qq.com>
2023-02-23 16:05:45 +0800

    init

---
+++ public/config.js
@@ -0,0 +1,23 @@
+window.APP_CONFIG = {
+  primaryColor: window.APP_PRIMARY_COLOR, // 替换主题色
+  logo: '', // 替换logo
+  logoDisabled: false, // 不显示logo
+  editLogoPreview: false,
+  APPTitle: '页面编排',
+  params: {
+    kgName: 'default_user_graph_default',
+    APK: '445a793dd0314abd875b7980d7ffbbd6',
+    nsId: 1,
+  },
+  menusAppList: [
+    {
+      label: 'KGSearch',
+      value: 'kg-search',
+      base: 'http://192.168.3.103:8081'
+    }
+  ]
+}
+
+// 自定义全局参数
+// 局部参数
+// search保存
